8ec2b62750dfe045bb1cddb37cccd2b60d8f20c7
Luiz Felipe Arjonilla de Mattos <65977358+Arjonilla87@users.noreply.github.com>
2026-01-31 11:23:01 -0300

    Update greeting from 'Hello' to 'Goodbye'

--- js/estatisticas.js
+++ js/estatisticas.js
@@ -401,6 +401,7 @@ async function loadCumulative() {
 
             scales: {
                 x: {
+                    max: Math.max(...datasets[1].data) * 1.1,  // aumenta 10% sobre o valor máximo de "Convocações"
                     ticks: {
                         color: "#000000",
                         font: {
@@ -427,7 +428,6 @@ async function loadCumulative() {
                     },
                     title: {
                         display: true,
-                        max: Math.max(...datasets[1].data) * 1.1,  // aumenta 10% sobre o valor máximo de "Convocações"
                         text: "Convocações",
                         color: "#000000",
                         font: {
@@ -1424,6 +1424,7 @@ function updateOptionsTable(selectedBucket) {
 
 // Chama o loader ao iniciar a página
 document.addEventListener("DOMContentLoaded", loadOptionsDistribution);
+
 
 
 
